838d2644056a4b3fc2664dbb60f632055808c3bf
freshSauce <nex0loke@gmail.com>
2022-02-06 00:01:50 -0600

    Modified routes and added User routes

--- routes/routes.py
+++ routes/routes.py
@@ -1,13 +1,19 @@
-from .serializers import *
+from routes.serializers import *
 
 
 def initialize_routes(api):
-    api.add_resource(GroupApiGet, '/group/<int:group_id>')
-    api.add_resource(GroupApiPost, '/group/<int:group_id>')
+    api.add_resource(GroupApiGet, '/get_info/<int:group_id>')
+    api.add_resource(GroupApiPost, '/add_group/<int:group_id>')
     api.add_resource(
-        GroupApiPut, '/group/<int:group_id>/<string:action>/<int:user_id>'
+        GroupApiPut, '/modify/<int:group_id>/<string:action>/<int:user_id>', 
+
     )
     api.add_resource(
         GroupApiDelete,
-        '/group/<int:group_id>/<string:action>/<int:user_id>'
+        '/remove/<int:group_id>/<string:action>/<int:user_id>',
     )
+    api.add_resource(UserApiGet, '/user_info/<int:user_id>')
+    api.add_resource(UserApiPost, '/add_user/<int:user_id>')
+    api.add_resource(UserApiPut, '/modify_user/<int:user_id>')
+    
+
