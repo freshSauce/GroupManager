5c6c39353cafc9e5127f5d4eab9b16b7e439d916
freshSauce <nex0loke@gmail.com>
2022-02-06 00:03:09 -0600

    Added API information

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import configparser
 from flask import Flask
 from flask_restx import Api
-
-from database.db import initialize_db
-from routes.routes import initialize_routes
+import os
 
 config = configparser.ConfigParser(inline_comment_prefixes=('#', ))
 
 config.read('config.ini')
 
+os.environ["MONGO_URI"] = config['CONFIG']['MONGO_URI']
+os.environ["API_KEY"] = config['CONFIG']['API_KEY']
+
+from database.db import initialize_db
+from routes.routes import initialize_routes
+
 app = Flask(__name__)
-api = Api(app)
+api = Api(
+    app,
+    title="Group Manager API",
+    version="1.0",
+    description="Group Manager API - Easy to use:)"
+)
 
 app.config['MONGODB_SETTINGS'] = {'host': config["CONFIG"]["MONGO_URI"]}
 
@@ -18,4 +27,4 @@ initialize_db(app)
 initialize_routes(api)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80)
